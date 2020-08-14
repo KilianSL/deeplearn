b207b0a6ee9620ec7bac86c5e93c4db60c0fdc0e
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-08-14 22:41:12 +0100

    Moved MatrixUtils to its own file for cleanlieness

---
+++ NEA/Tensor/MatrixUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEA.Tensor
+{
+    static class MatrixUtils
+    {
+        public static float[] RandomGaussian(float mean, float stdDev) // Returns an array of 2 random gaussian variables generated using the Marsaglia-Polar method
+        {
+            Random random = new Random();
+            float randomFloat() { return (float)random.NextDouble(); } // gets a random float value from U(0,1)
+            float s = 5;
+            float u1 = 1;
+            float u2 = 1;
+            while (s > 1)
+            {
+                u1 = randomFloat();
+                u2 = randomFloat();
+                u1 = 2 * u1 - 1;
+                u2 = 2 * u2 - 2;
+                s = MathF.Pow(u1, 2) + MathF.Pow(u2, 2);
+            }
+            u1 = MathF.Sqrt((-2 * MathF.Log(s)) / s) * u1;
+            u2 = MathF.Sqrt((-2 * MathF.Log(s)) / s) * u2;
+            return new float[] { u1 * stdDev + mean, u2 * stdDev + mean };
+        }
+    }
+}
