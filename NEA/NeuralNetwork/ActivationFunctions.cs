3fef50ff0ba148307c397a5f86d73edf0f078e79
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2021-03-20 23:10:17 +0000

    Moved LossFunction and ActivationFunctions to NEA.NeuralNetwork namespace. Deleted associated folders.

--- NEA/NeuralNetwork/ActivationFunctions.cs
+++ NEA/NeuralNetwork/ActivationFunctions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace NEA.NeuralNetwork.ActivationFunctions
+namespace NEA.NeuralNetwork
 {
     using Tensor;
 
---
+++ NEA/NeuralNetwork/LossFunctions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NEA.NeuralNetwork
+{
+    public class LossFunctions
+    {
+    }
+}
